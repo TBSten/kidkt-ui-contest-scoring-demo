f37bf62d3026648a058e9fbc21813a8ead6b8479
TBSten <81161390+TBSten@users.noreply.github.com>
2024-03-25 05:32:23 +0900

    サンプルのテストを追加

--- app/src/test/java/me/tbsten/kidktuicontestscoringdemo/ExampleUnitTest.kt
+++ app/src/test/java/me/tbsten/kidktuicontestscoringdemo/ExampleUnitTest.kt
@@ -1,17 +1,52 @@
 package me.tbsten.kidktuicontestscoringdemo
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import com.github.takahirom.roborazzi.ExperimentalRoborazziApi
+import com.github.takahirom.roborazzi.RoborazziOptions
+import com.github.takahirom.roborazzi.captureRoboImage
+import org.junit.Rule
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.GraphicsMode
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@RunWith(RobolectricTestRunner::class)
 class ExampleUnitTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @OptIn(ExperimentalRoborazziApi::class)
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun roborazziTest() {
+        val inputUserName = "tbsten"
+        val inputScore = 12345
+
+        composeTestRule.setContent {
+            Box(
+                modifier = Modifier
+                    .testTag("root"),
+            ) {
+                ScoreSection(
+                    user = inputUserName,
+                    score = inputScore,
+                )
+            }
+        }
+
+        composeTestRule
+            .onNodeWithTag("root")
+            .assertExists("rootSurfaceタグの付与されたSurfaceが見つかりません。指定されたComposable以外を編集しないでください。")
+            .captureRoboImage("screenshots/$inputUserName.png", RoborazziOptions())
+
     }
 }
